a333c1dd1101b3614a9acd3ea0b28d8ea814c44a
ntnfolla <ntnfolla@gmail.com>
2022-04-07 11:33:07 -0700

    UNIX is not hard

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -7,6 +8,7 @@
  */
 int main(void)
 {
-write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+write(STDOUT_FILENO, "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
