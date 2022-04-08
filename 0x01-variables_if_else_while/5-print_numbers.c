25197e529f73e11c2af71744d3a6530d991dff10
ntnfolla <ntnfolla@gmail.com>
2022-04-08 09:03:26 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = '0'; i < '10'; ++i)
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
