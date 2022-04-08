dd5458fe28399b4d16b1e28281d18f2b04432fa8
ntnfolla <ntnfolla@gmail.com>
2022-04-08 08:27:07 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print the lowercase alphabet and Uppercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+for (c = 0; c < 26; ++c)
+putchar('A' + c);
+
+putchar('\n');
+
+return (0);
+}
