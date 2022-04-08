e5ac5ec4f5ca5d02f9118fea6ae7f3f4e3550bc3
ntnfolla <ntnfolla@gmail.com>
2022-04-08 09:38:19 -0700

    haxadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@ int main(void);
 {
 char x;
 
-for (x = '0'; x < '9'; ++x)
+for (x = '0'; x <= '9'; ++x)
 putchar(x);
-for (x = 'a'; x<= 'f'; ++x)
+for (x = 'a'; x <= 'f'; ++x)
 putchar(x);
 
 putchar('\n')
