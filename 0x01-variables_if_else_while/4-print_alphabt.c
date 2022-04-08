eb887148ec1aaba9d91e9484ab591c65e9b91426
ntnfolla <ntnfolla@gmail.com>
2022-04-08 08:44:08 -0700

    the alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print lowercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 5; c > 5; c < 18; c > 18; c < 26; ++c)
+putchar('a' + c);
+
+putchar('\n');
+
+return (0);
+}
