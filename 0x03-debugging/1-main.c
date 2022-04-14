74c6d0f6c58461818cf912017632f7991734a0bd
ntnfolla <ntnfolla@gmail.com>
2022-04-13 23:50:34 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,13 +7,18 @@
 
 int main(void)
 {
-int i
+int i;
+
 printf("infinite loop incoming :(\n");
+
 i = 0;
-while (i < 10)
-{
-putchar(i);
-}
+
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
