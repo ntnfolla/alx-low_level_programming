1ce1c7eadc0968a201ea87a2c94fff0467c3e049
ntnfolla <ntnfolla@gmail.com>
2022-04-14 08:47:44 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if it is digit
+ * @c: input number
+ * Return: give 1 if it is a number (0 and 9), 0 otherwise
+ */
+
+int _isdigit(int c);
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
