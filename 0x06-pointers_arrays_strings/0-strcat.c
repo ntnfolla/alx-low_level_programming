e5f3086086a77fcbed86de7c35c437530723d199
ntnfolla <ntnfolla@gmail.com>
2022-04-20 22:55:31 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_strcat - concatenates "src" and "dest"
+ *_strcat - concatenates two strings
  *@dest: pointer to destnation input
  *@src: pointer to source input
  *
@@ -9,14 +9,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int c, c2;
-c = 0;
-/*find the size of dest array*/
-while (dest[c])
-C++;
-/* iterate through each src array value without the null byte*/
-for (c2 = 0; src[c2] ; c2++)
-/*append src[c2] to dest [c] while overwritting the null byte in dest*/
-dest[c++] = src[c2];
+int abebe = 0, i;
+while (dest[abebe])
+{
+abebe++;
+}
+for (i = 0; src[i] != 0; i++)
+{
+dest[abebe] = src[i];
+abebe++;
+}
+dest[abebe] = '\0';
 return (dest);
 }
