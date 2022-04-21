55c8650de9e642b428992ce279d37718124c4eaa
ntnfolla <ntnfolla@gmail.com>
2022-04-21 02:13:25 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,17 @@
  */
 void print_number(int n)
 {
-unsigned int x;
 if (n < 0)
 {
 _putchar('-');
-n *= -1;
+if (n < -9)
+	print_number(n / -10);
+_putchar('0' - n % 10);
 }
-x = n;
-if (x / 10)
-	print_number(x / 10);
-_putchar(x % 10 + '0');
+else
+{
+if (n > 9)
+	print_number(n / 10);
+_putchar(n % 10 + '0');
+}
 }
