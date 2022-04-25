2774bd034b51ddd0fee066bdb46f66c0b5fbf818
ntnfolla <ntnfolla@gmail.com>
2022-04-25 08:48:48 -0700

    task 0 memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills with constant byte
+ * @s: one constant
+ * @char b: second constant
+ * @n: size
+ *
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+while (i < n)
+{
+*(s + i) = b;
+i++;
+}
+Return (s);
+}
