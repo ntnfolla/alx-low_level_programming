276b68567cddb5fa7e9386d1d5074e50a1079161
ntnfolla <ntnfolla@gmail.com>
2022-04-25 13:30:24 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - redirect
+ * @s: double pointer
+ * @to: main pointer
+ *
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
