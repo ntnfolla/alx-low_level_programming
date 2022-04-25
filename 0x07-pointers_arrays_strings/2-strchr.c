4c7048078716dad4c340706047b5e13dd301212f
ntnfolla <ntnfolla@gmail.com>
2022-04-25 09:14:14 -0700

    task 2 strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates a character
+ * @s: first character
+ * @c: second character
+ *
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+
+else
+return (0);
+}
+}
+}
