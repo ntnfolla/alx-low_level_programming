c1e689664a08dd7075e9e5186bba184fc65b1212
ntnfolla <ntnfolla@gmail.com>
2022-04-29 09:27:49 -0700

    all argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints all argumrnt
+ * @argc: Number of command line
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+
+return (0);
+}
