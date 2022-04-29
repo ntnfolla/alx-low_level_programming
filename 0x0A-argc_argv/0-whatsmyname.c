06394eecfed916c3978c3b681b01057a49bb74a5
ntnfolla <ntnfolla@gmail.com>
2022-04-29 07:42:09 -0700

    whats my name?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints my name
+ * @argc: number of commands
+ * @argv: array
+ * Return: 0
+ */
+int main (int argc, char* argv[])
+{
+printf("%s\n", argv[argc * 0]);
+
+return (0);
+}
