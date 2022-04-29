24f1a09bd95e2fe96e2b1acf99ab6f1a542f51f2
ntnfolla <ntnfolla@gmail.com>
2022-04-29 07:46:45 -0700

    whats my name?

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - prints my name
  * @argc: number of commands
- * @argv: array
+ * @argv: array name
  * Return: 0
  */
-int main (int argc, char* argv[])
+
+int main (int argc, char *argv[])
 {
 printf("%s\n", argv[argc * 0]);
 
