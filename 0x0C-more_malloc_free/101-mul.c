d86365c58931a54ff525aca67a4a27908bb5b776
ntnfolla <ntnfolla@gmail.com>
2022-05-06 19:19:30 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * Error - prints Error
+ * Error - prints Error str
+ *
  * Return: Error and new line
  */
 void Error(void)
@@ -23,7 +24,7 @@ exit(98);
 int allDigits(char **arg)
 {
 int i, j;
-for (i = 1, i <= 2, i++)
+for (i = 1; i <= 2; i++)
 for (j = 0; arg[i][j]; j++)
 {
 if (arg[i][j] < '0' || arg [i][j] > '9')
@@ -35,6 +36,7 @@ return (1);
  * _calloc - initializes memory spaces with zero
  * @nmemb: string 1
  * @size: string 2, concatenated to 1
+ *
  * Return: pointer to the concatenated string
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -42,10 +44,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 unsigned int i;
 char *newArray;
 if (nmemb == 0 || size == 0)
-	return (NULL);
+return (NULL);
 newArray = malloc(nmemb * size);
 if (newArray == NULL)
-	return (NULL);
+return (NULL);
 for (i = 0; i < (nmemb * size); i++)
 *(newArray + i) = 0;
 return (newArray);
@@ -63,23 +65,23 @@ char *s1 = *(argv + 1), *s2 = *(argv + 2);
 int *a, *b, *ans;
 
 if (argc != 3 || allDigits(argv) != 1)
-	Error();
+Error();
 if (*s1 == '0' || *s2 == '0')
-	_putchar('0');
+_putchar('0');
 while (*(*(argv + 1) + len_s1))
-	len_s1++;
+len_s1++;
 while (*(*(argv + 2) + len_s2))
-	len_s2++;
+len_s2++;
 len = len_s1 + len_s2 + 1;
 a = (int *) malloc(len_s1 *sizeof(int));
 b = (int *) malloc(len_s2 *sizeof(int));
 ans = _calloc(len, sizeof(int));
 if (a == NULL || b == NULL || ans == NULL)
-	Error();
+Error();
 for (i = len_s1 - 1, j = 0; i >= 0; i--, j++)
-	*(a + j) = *(s1 + i) - '0';
+*(a + j) = *(s1 + i) - '0';
 for (i = len_s2 - 1, j = 0; i >= 0; i--, j++)
-	*(b + j) = *(s2 + i) - '0';
+*(b + j) = *(s2 + i) - '0';
 for (i = 0; i < len_s2; i++)
 for (j = 0; j < len_s1; j++)
 *(ans + i + j) = *(ans + i + j) + *(b + i) * *(a + j);
