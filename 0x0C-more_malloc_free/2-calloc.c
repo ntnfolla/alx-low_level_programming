0ec527a5fb62d65a532e9822bed1833968a49b2b
ntnfolla <ntnfolla@gmail.com>
2022-05-06 17:58:27 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmeb: string one
+ * @size: string two, concatenates to 1
+ * Return: pointer to the concatenated string
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *newArray;
+
+if (nmeb == 0 || size == 0)
+	return (NULL);
+newArray = malloc(nmemb * size);
+if (newArray == NULL)
+	return (NULL);
+for (i = 0; i < (nmemb * size); i++)
+{
+*(newArray + i) = 0;
+}
+return (newArray);
+}
