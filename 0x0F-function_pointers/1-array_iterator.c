5986d98dc6bb8090c4658a03ef501c04087706f4
ntnfolla <ntnfolla@gmail.com>
2022-05-11 12:29:01 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "sunction_pointers.h"
+/**
+ * array_iterator - executes a function
+ * @array: array
+ * @size: the size of array
+ * @action: a pointer to the function
+ * Return: 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (array == NULL || action == NULL)
+	return;
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
+}
