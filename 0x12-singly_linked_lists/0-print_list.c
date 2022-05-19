845753351763b2855e103d236206e96711b68ae5
ntnfolla <ntnfolla@gmail.com>
2022-05-19 12:13:27 -0700

    fixes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_list - prints all the ellements of a list
+ * print_list - is a function that prints all the ellements of a list
  * @h: the string input
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
-
-while (h)
-{
-if (h->str == NULL)
-	printf("[%d] (%s)\n", 0, "nil");
-else
-{
-printf("[%d] (%s)\n", h->len, h->str);
-}
-h = h->next;
-count++;
-}
-return (count);
+	size_t count = 0;
+	
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[%d] (%s)\n", 0, "nil");
+		else
+		{
+			printf("[%d] (%s)\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
