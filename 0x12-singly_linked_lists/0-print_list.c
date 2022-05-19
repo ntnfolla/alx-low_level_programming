75651a70455cc04355b34c791a93b8f27d5ef0c4
ntnfolla <ntnfolla@gmail.com>
2022-05-19 08:44:41 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "list.h"
+
+/**
+ * print_list - prints all the ellements of a list
+ * @h: the string input
+ * Return: NULL
+ */
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+while (h)
+{
+if (h-str == NULL)
+	printf("[%d] (%s)\n", 0, "nil");
+else
+{
+printf("[%d] (%s)\n", h->len, h->str);
+}
+h = h->next;
+count++;
+}
+return (count);
+}
