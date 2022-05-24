9bf91ba226dbe335c7b8c4b4b23f00b99f79eda8
ntnfolla <ntnfolla@gmail.com>
2022-05-24 09:37:09 -0700

    task 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,7 +3,7 @@
 /**
  * find_listint_loop - find the loop in a linked list
  * @head: head of linked list
- * Description: Not allowed to use malloc, free or arrays.
+ * Description: Not allowed to use malloc, free or arrays
  * Can only declare a max of 2 variables.
  * Return: Address of node where loop starts, or NULL if no loop found.
  */
