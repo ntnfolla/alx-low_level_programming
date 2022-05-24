91f55ba584f19eaa37f6e72e3e2e5b375b153363
ntnfolla <ntnfolla@gmail.com>
2022-05-24 09:33:59 -0700

    task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,7 +3,9 @@
 /**
  * reverse_listint - Reverse a `listint_t` linked list
  * @head: double pointer to head
- * Description: Limited to going through loop only once. Not allowed to use `malloc` or `free` or arrays can only declare a max of 2 variables.
+ * Description: Limited to going through loop only once.
+ * Not allowed to use `malloc` or `free` or arrays
+ * can only declare a max of 2 variables.
  * Return: pointer to first node of reversed list
  */
 listint_t *reverse_listint(listint_t **head)
