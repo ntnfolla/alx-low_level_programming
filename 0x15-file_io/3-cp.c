2699db9c7801bec26be840a26cd05dc90e5edd99
ntnfolla <ntnfolla@gmail.com>
2022-05-30 11:53:10 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
-void closer(int arg_files);
+#ifndef BUFF_SIZE
+#define BUFF_SIZE 1024
+#endif
 
 /**
- * main - Entry Point
- * @argc: # of args
- * @argv: array pointer for args
- * Return: 0
+ * main - check the code for Holberton School students.
+ * @argc: name of my file
+ * @argv: number of the letters that i used
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-int file_from, file_to, file_from_r, wr_err;
-char buf[1024];
+int file_origin, file_dest;
+ssize_t numb;
+char buff[BUFF_SIZE];
 if (argc != 3)
 {
-dprintf(2, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
+file_origin = open(argv[1], O_RDONLY);
+if (file_origin == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-if (file_to == -1)
+file_dest = open(argv[2], O_CREAT | O_WRONLY, 0664);
+if (file_dest == -1)
 {
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
-while (file_from_r >= 1024)
+while ((numb = read(file_origin, buff, BUFF_SIZE)) > 0)
 {
-file_from_r = read(file_from, buf, 1024);
-if (file_from_r == -1)
+if (write(file_dest, buff, numb) != numb)
 {
-dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-closer(file_from);
-closer(file_to);
-exit(98);
-}
-wr_err = write(file_to, buf, file_from_r);
-if (wr_err == -1)
-{
-dprintf(2, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
-closer(file_from);
-closer(file_to);
-return (0);
+if (numb == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
-
-/**
- * closer - close with error
- * @arg_files: argv 1 or 2
- * Return: void
- */
-void closer(int arg_files)
+if ((close(file_origin) == -1) || (close(file_dest) == -1))
 {
-int close_err;
-close_err = close(arg_files);
-if (close_err == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", arg_files);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_origin);
 exit(100);
 }
+exit(EXIT_SUCCESS);
 }
