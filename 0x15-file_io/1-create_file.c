7f2e9acaa65ef1a559ffbecd406d4144147177e5
ntnfolla <ntnfolla@gmail.com>
2022-05-30 10:31:53 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
  * create_file - Entry Point
  * @filename: file name
