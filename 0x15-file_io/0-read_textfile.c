ace8100fe72af7d582602699b48ae9774ebbb862
ntnfolla <ntnfolla@gmail.com>
2022-05-30 10:11:15 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,40 +5,40 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-/*
- * read_textfile - write a function that reads a text file and prints it to POSIX
- * @filename: character for filename
- * return: 0
+/**
+ * read_textfile - Entry Point
+ * @filename: file name
+ * @letters: size
+ * Return: 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd, i, j;
+int file, rd, wr;
 char *buf;
-if (!filename)
-	return (0);
-fd = open(filename, O_RDONLY);
-if(fd == -1)
-	return (0);
-buf = malloc(sizeof(char) * letters);
 
-if (!buf)
-	return(0);
-i = read(fd, buf, letters);
-if (i < 0)
-{
-free(buf);
+if (filename == NULL)
 return (0);
-}
-buf[i] = '\0';
-close(fd);
-j = write(STOUT_FILENO, buf, i);
-if (j < 0)
-{
-free(buf);
-return (0);
-}
-free(buf);
 
-return (j);
+file = open(filename, O_RDONLY);
+
+if (file == -1)
+return (0);
+
+buf = malloc(sizeof(char) * letters + 1);
+if (buf == NULL)
+return (0);
+
+rd = read(file, buf, letters);
+if (rd == -1)
+return (0);
+
+buf[letters] = '\0';
+
+wr = write(1, buf, rd);
+if (wr == -1)
+return (0);
+
+close(file);
+free(buf);
+return (wr);
 }
