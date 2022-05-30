ddeca88e32f685246e45a5e2bfcc5b4ce907d18a
ntnfolla <ntnfolla@gmail.com>
2022-05-30 13:20:23 -0700

    task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,31 +1,48 @@
-#include <elf.h>
-#include <fcntl.h>
+#include "holberton.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+
+#define MAXSIZE 1204
+#define SE STDERR_FILENO
 
 /**
- * main - is a function that prints opcodes of this function
- * @argc: the number of arguments
- * @argv: the arrat of arguments
- * Return: Always 0
+ * main - create the copy bash script
+ * @ac: argument count
+ * @av: arguments as strings
+ * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
-int i;
-char *mem;
-if (argc != 2)
+int input_fd, output_fd, istatus, ostatus;
+char buf[MAXSIZE];
+mode_t mode;
+mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+if (ac != 3)
+dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
+input_fd = open(av[1], O_RDONLY);
+if (input_fd == -1)
+dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
+output_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+if (output_fd == -1)
+dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
+do {
+istatus = read(input_fd, buf, MAXSIZE);
+if (istatus == -1)
 {
-printf("Error\n");
-exit(1);
+dprintf(SE, "Error: Can't read from file %s\n", av[1]);
+exit(98);
 }
-i = atoi(argv[1]);
-if (i < 0)
+if (istatus > 0)
 {
-printf("Error\n");
-exit(2);
+ostatus = write(output_fd, buf, (ssize_t) istatus);
+if (ostatus == -1)
+dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
 }
-while (i--)
-printf("%02x%c", *mem++ & 0xff, i ? ' ' : '\n');
+} while (istatus > 0);
+istatus = close(input_fd);
+if (istatus == -1)
+dprintf(SE, "Error: Can't close fd %d\n", input_fd), exit(100);
+ostatus = close(output_fd);
+if (ostatus == -1)
+dprintf(SE, "Error: Can't close fd %d\n", output_fd), exit(100);
 return (0);
 }
