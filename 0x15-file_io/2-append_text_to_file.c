9ebd861dc0fb66b4d1a8c3bd00b76a7bea6ef5bd
ntnfolla <ntnfolla@gmail.com>
2022-05-30 12:41:51 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
+
 /**
- * append_text_to_file - Entry Point
- * @filename: file name
- * @text_content: text content
- * Return: 1
+ * append_text_to_file - check the code for Holberton School students.
+ * @filename: name of my file
+ * @text_content: number of the letters that i used
+ * Return: Always 0.
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-int file, wr, i = 0;
+int ID = 0, wr = 0, count = 0;
 if (filename == NULL)
-return (-1);
-file = open(filename, O_RDWR | O_APPEND);
-if (file == -1)
-return (-1);
-while (text_content[i])
-i++;
-if (text_content == NULL)
 {
-close(file);
-return (1);
+return (-1);
 }
-else
+if (text_content != NULL)
 {
-wr = write(file, text_content, i);
+for (count = 0; text_content[count] != '\0'; count++)
 }
+ID = open(filename, O_APPEND | O_WRONLY, 0600);
+if (ID == -1)
+{
+return (-1);
+}
+wr = write(ID, text_content, count);
 if (wr == -1)
+{
 return (-1);
-close(file);
+}
+close(ID);
 return (1);
 }
