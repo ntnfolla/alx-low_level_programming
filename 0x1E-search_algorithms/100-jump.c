4a12d8c3ae3c32a638ce029e4446fcef7ee3752c
ntnfolla <ntnfolla@gmail.com>
2022-11-01 07:06:08 -0700

    update

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,8 +2,7 @@
 #include <math.h>
 
 /**
- * jump_search - searches for a value in an array of
- * integers using the Jump search algorithm
+ * jump_search - search for a value in an array that integers using Jump search algorithm
  *
  * @array: input array
  * @size: size of the array
